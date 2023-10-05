ff1e566a7f82bc628b5c5695a32d02074b719e4c
nir-programmer <niritzhak10@gmail.com>
2023-10-05 11:04:47 +0300

    FINISH UI PAGES

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
@@ -21,15 +22,43 @@ function Single() {
             <p>Posted 2 days ago</p>
           </div>
           <div className="edit">
-            <Link to={`/write?edit?2`}>
+            <Link to={`/write?edit=2`}>
               <img src={Edit} alt="" />
             </Link>
 
             <img src={Delete} alt="" />
           </div>
         </div>
+        <h1>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident,
+          eius.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio velit
+          unde vitae maxime quibusdam quis beatae, voluptates distinctio nam
+          voluptatibus dolorum at numquam, a nulla commodi atque cumque natus
+          inventore?
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          dicta magnam nulla ut delectus amet soluta. Sequi quia suscipit
+          molestias, qui quod quam repellendus veniam unde dolorem. Consequuntur
+          dolorem ut sed, cumque fugiat magnam quae possimus alias numquam sit
+          minima, optio labore natus eum eligendi quod dolores veritatis
+          deleniti non. Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Vel, voluptas amet. Est ipsum vero fuga laborum harum quasi
+          soluta animi consectetur incidunt quod consequatur explicabo ea
+          exercitationem non accusamus obcaecati amet tempore ipsa accusantium,
+          excepturi ullam voluptate. Aspernatur sapiente facere dolores fugiat!
+          Ipsam esse veniam illo, qui accusantium, quidem vel quam animi dolor
+          maiores veritatis aspernatur sequi ut velit rerum? Repellat cupiditate
+          veniam totam vero id asperiores ipsa eveniet, velit sunt laudantium
+          molestiae ducimus cum laboriosam iste pariatur aspernatur harum quia
+          obcaecati? Dolorum incidunt illum ut facere temporibus cum ullam
+          molestias, accusantium possimus nobis pariatur magni corrupti quisquam
+          omnis hic!
+        </p>
       </div>
-      <div className="menu">MENU</div>
+      <Menu></Menu>
     </div>
   );
 }
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,5 +1,78 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 function Write() {
-  return <div>Write</div>;
+  //QUILL STATE(DOCS)
+  const [value, setValue] = useState("");
+
+  //Test Quill Library - chack the change of the state
+  console.log(value);
+  return (
+    <div className="add">
+      <div className="content">
+        <input type="text" placeholder="Title" />
+        <div className="editorContainer">
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          ></ReactQuill>
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b>Status:</b>Draft
+          </span>
+          <span>
+            <b>Visibility:</b>Public
+          </span>
+          <input style={{ display: "none" }} type="file" name="" id="file" />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
+          <div className="buttons">
+            <button>Save as a draft</button>
+            <button>Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          <div className="cat">
+            <input type="radio" name="cat" id="art" value="art" />
+            <label htmlFor="art">Art</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" id="science" value="science" />
+            <label htmlFor="science">Science</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" id="tech" value="tech" />
+            <label htmlFor="tech">Tech</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" id="cinema" value="cinema" />
+            <label htmlFor="cinema">Cinema</label>
+          </div>
+
+          <div className="cat">
+            <input type="radio" name="cat" id="design" value="design" />
+            <label htmlFor="design">Design</label>
+          </div>
+          <div className="cat">
+            <input type="radio" name="cat" id="food" value="food" />
+            <label htmlFor="food">Food</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Write;
